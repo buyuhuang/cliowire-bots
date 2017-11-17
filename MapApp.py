998e78f98eb33e3eeefc9cd95ffba1a805d0109e
adescombes <albane.descombes@epfl.ch>
2017-11-17 10:00:31 +0100

    partie pour afficher des markers

--- MapApp.py
+++ MapApp.py
@@ -1,1 +1,14 @@
+import folium
 
+venice_coord = [45.4,12.33]
+m = folium.Map(location=venice_coord)
+venice = "Venice"
+
+# créer un marker avec lequel une box apparait qui contient une url
+
+doges_location = [45.4337, 12.3404]
+url = "<a href=\"https://cliowire.dhlab.epfl.ch/web/statuses/99018727713161107\">"+venice+"</a>"
+folium.Marker(doges_location, popup=url).add_to(m)
+
+m.save('MapAppTest.html')
+m
