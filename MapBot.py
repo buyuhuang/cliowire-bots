613e43c71b1874112c12bd3dfdb1609c6816a2bb
cedricviaccoz <cedric.viaccoz@gmail.com>
2017-11-20 13:57:49 +0100

    WIP MapBot implementation

--- MapBot.py
+++ MapBot.py
@@ -2,12 +2,17 @@ from clioServer import credentials, getPulses, postPulses
 from mastodon import Mastodon
 import sys
 import os
+import json
 
 APP_NAME = 'MapBot'
 BOT_LOGIN = 'viaccoz'
 BOT_PSWD = 'reallygoodpassword'
 HASH_MARKER = '#geoCoords'
-FINAL_PULSE = 'Today, {0} pulses were geoparsed and then added to the map of GeoPulses !'
+FINAL_PULSE = 'Today, {} pulses were geoparsed and then added to the map of GeoPulses !'
+
+GEOJSON_PRE = "{\"type\": \"FeatureCollection\",\"generator\": \"overpass-turbo\",\"copyright\": \"2017, EPFL \",\"timestamp\": \"2017-11-20T13:03:02Z\",\"features\": ["
+
+GEOJSON_POST = "]}"
 
 def main(args):
 
@@ -17,41 +22,49 @@ def main(args):
 
     geopulses = getPulses.retrieve(hashtag=HASH_MARKER)
 
-    geoJsonFile = getGeoPulsesFile()
+    geoJsonFile = openGeoPulsesFile()
 
     toWrite = ''
 
+
     for p in geopulses:
         pJson = jsonParse(p)
-        toWrite.append(pJson)
+        toWrite.append(pJson+',')
 
-    geoJsonFile.write(toWrite)
+    nmbOfPulses = len(geopulses)
 
-    postPulses.post_content(cliowireConn, FINAL_PULSE.format(len(geopulses)))
+    if nmbOfPulses >0:
+        toWrite = toWrite[:-1]
+    geoJsonFile.write(toWrite.append(GEOJSON_POST))
 
-
-
-
-def getGeoPulsesFile():
-    #TODO open the file of geoJson entities in append mode, if it does no exist,
+    postPulses.post_content(cliowireConn, FINAL_PULSE.format(nmbOfPulses))
 
 
-"""
-Format of the geopulse (in JSON):
-{
-    "type": "Feature",
-    "geometry": {
-        "type": "Point",
-        "coordinates": [12.3404, 45.4337]
-    },
-    "properties": {
-        "pulseid": "99018727713161107",
-        "content": "DHstudents went to Venice",
-        "entities": ["Venice", "DHstudents"]
-    }
-}
-"""
+
+
+def openGeoPulsesFile():
+    #TODO open the file of geoJson entities in append mode (which means it ), if it does no exist, create it.
+    pass
+
+def contentBreakDown(content):
+    #TODO : parse content to extract the coordinates, the named entities, and a clean version of the pulse's content (without balises, and named entitities URL)
+    pass
+
 def jsonParse(pulse):
-    #TODO fetch the coordinate from the pulse, detect named entities, and write it in the geoJsonPulse format.
+    content, entities, coordinates = contentBreakDown(pulse["content"])
+    pulseId = str(pulse["id"])
+    #this below is the jsonGeoPulse format we're using. Coordinates must be a list/array of 2 floats, entities a list/array of string, everything else Strings.
+    return json.dumps({
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": coordinates
+        },
+        "properties": {
+            "pulseid": pulseId,
+            "content": content,
+            "entities": entities
+        }
+    })
 
 main(sys.argv)
